42517da9a2fcb78e867f3be1ea1cdc10b4363450
damartinLee <93048929+MangMuanMung@users.noreply.github.com>
2025-11-17 15:58:26 +0700

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Cybersecurity Analyst", "SOC Specialist", "Threat Hunter", "Blue Team Defender"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
